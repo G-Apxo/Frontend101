e8b28dda9129063be085d88a9ac2932c88311709
Giorgi Aphkhadze <72552435+G-Apxo@users.noreply.github.com>
2024-02-13 21:54:28 +0400

    pokedex

--- javascript/pokedex/description.js
+++ javascript/pokedex/description.js
@@ -1,11 +1,6 @@
-const poke_container = document.getElementById("poke_contianer");
-const pokemon_count = 150;
+const id = new URLSearchParams(window.location.search).get("id");
 
-const getPokemons = async () => {
-  for (let i = 1; i <= pokemon_count; i++) {
-    await fetchPokemons(i);
-  }
-};
+const poke_container = document.getElementById("poke_contianer");
 const colours = {
   normal: "#A8A77A",
   fire: "#EE8130",
@@ -27,27 +22,28 @@ const colours = {
   fairy: "#D685AD",
 };
 const main_types = Object.keys(colours);
+
 const fetchPokemons = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
+  console.log(data);
   createPokemonCard(data);
 };
+fetchPokemons(id);
 
 const createPokemonCard = (pokemon) => {
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
   const poke_types = pokemon.types.map((type) => type.type.name);
   const type = main_types.find((type) => poke_types.indexOf(type) >= 0);
-  console.log(type);
-  pokemonEl.classList.add(type);
   const id = pokemon.id.toString().padStart(3, "0");
   const color = colours[type];
   pokemonEl.style.backgroundColor = color;
   const prokemonInnerHTML = `
-    <div class="img-container">
-      <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
-      </div>
+        <div class="img-container">
+          <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+        </div>
         <div class="info">
           <span class="number">#${id}</span>
           <h3 class="name">${pokemon.name}</h3>
@@ -59,4 +55,3 @@ const createPokemonCard = (pokemon) => {
   pokemonEl.innerHTML = prokemonInnerHTML;
   poke_container.appendChild(pokemonEl);
 };
-getPokemons();
